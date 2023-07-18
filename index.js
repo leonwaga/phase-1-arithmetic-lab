c86f11fdedd698ccb313fd43e1c72e74e4b43eda
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-07-18 09:51:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+function multiply(num1,num2) {
+    let result = num1*num2; return result;
+}
+console.log (multiply(31, 2));
 
-// Write your code here
+function random() {
+    return Math.floor(Math.random() * 100);
+    
+}
+console.log (random());
+
+function mod(num3,num4) {
+    let result = num3%num4; return result;
+}
+console.log (mod(25, 7));
+
+const numbers = [10, 5, 20, 15, 8];
+const max = Math.max(...numbers);
+
+console.log(max);
+
+
+
+  
